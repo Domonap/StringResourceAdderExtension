8ac488dfd8dd3a2360e026d8f87328a69c95613a
Gia Kvaratskheliya <gia@domonap.ru>
2020-05-08 16:15:20 +0300

    Refactoring

--- AddResourcesCommand.cs
+++ AddResourcesCommand.cs
@@ -1,266 +1,243 @@
-﻿using EnvDTE;
-using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Linq;
 using System.Xml.Linq;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using IAsyncServiceProvider = Microsoft.VisualStudio.Shell.IAsyncServiceProvider;
 using Task = System.Threading.Tasks.Task;
 
 namespace StringResourceAdderExtension
 {
     internal sealed class AddResourcesCommand
     {
-
-        #region Fields
-        private static readonly XNamespace X = "http://schemas.microsoft.com/winfx/2006/xaml";
-        private static readonly XNamespace XML = "http://www.w3.org/XML/1998/namespace";
-        private static readonly XName SPACE = XName.Get("space", XML.NamespaceName);
-        private static readonly XName UID = XName.Get("Uid", X.NamespaceName);
         
-        private static readonly XName TEXT = XName.Get("Text");
-        private static readonly XName HEADER = XName.Get("Header");
-        private static readonly XName CONTENT = XName.Get("Content");
-        private static readonly XName TOOLTIP = XName.Get("ToolTipService.ToolTip");
-        List<XName> HEADERS = new List<XName>() { HEADER, CONTENT, TOOLTIP, TEXT };
+       #region Fields
 
-        private static readonly XName ROOT = XName.Get("root");
-        private static readonly XName NAME = XName.Get("name");
-        private static readonly XName VALUE = XName.Get("value");
-        private static readonly XName COMMENT = XName.Get("comment");
-        private static readonly XName DATA = XName.Get("data");
-        List<ProjectItem> projectItems = new List<ProjectItem>();
-        int keywordsCount;      
-        #endregion Fields 
+        private static readonly XNamespace X = "http://schemas.microsoft.com/winfx/2006/xaml";
+        private static readonly XNamespace Xml = "http://www.w3.org/XML/1998/namespace";
+        private static readonly XName Space = XName.Get("space", Xml.NamespaceName);
+        private static readonly XName Uid = XName.Get("Uid", X.NamespaceName);
+
+
+        private static readonly XName PlaceholderText = XName.Get("PlaceholderText");
+        private static readonly XName Text = XName.Get("Text");
+        private static readonly XName Header = XName.Get("Header");
+        private static readonly XName Content = XName.Get("Content");
+        private static readonly XName Tooltip = XName.Get("ToolTipService.ToolTip");
+        private readonly List<XName> _headers = new List<XName> {Header, Content, Tooltip, Text, PlaceholderText};
+
+        private static readonly XName Root = XName.Get("root");
+        private static readonly XName Name = XName.Get("name");
+        private static readonly XName Value = XName.Get("value");
+        private static readonly XName Comment = XName.Get("comment");
+        private static readonly XName Data = XName.Get("data");
+        private readonly List<ProjectItem> _projectItems = new List<ProjectItem>();
+        private int _keywordsCount;
+
+        #endregion Fields
 
         #region Command Logic
 
         /// <summary>
-        /// Command ID.
+        ///     Command ID.
         /// </summary>
         public const int CommandId = 0x0100;
 
         /// <summary>
-        /// Command menu group (command set GUID).
+        ///     Command menu group (command set GUID).
         /// </summary>
         public static readonly Guid CommandSet = new Guid("136ad7ed-5e73-474a-bafa-8afd7fd356b0");
 
-        private readonly AsyncPackage package;
+        private readonly AsyncPackage _package;
 
         private AddResourcesCommand(AsyncPackage package, OleMenuCommandService commandService)
         {
-            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            _package = package ?? throw new ArgumentNullException(nameof(package));
             commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
 
-            CommandID menuCommandID = new CommandID(CommandSet, CommandId);
-            MenuCommand menuItem = new MenuCommand(Execute, menuCommandID);
+            var menuCommandId = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(Execute, menuCommandId);
             commandService.AddCommand(menuItem);
         }
 
 
-        public static AddResourcesCommand Instance
-        {
-            get;
-            private set;
-        }
+        public static AddResourcesCommand Instance { get; private set; }
 
-        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider => package;
+        private IAsyncServiceProvider ServiceProvider => _package;
 
         public static async Task InitializeAsync(AsyncPackage package)
         {
-            // Switch to the main thread - the call to AddCommand in SampleCommand's constructor requires
-            // the UI thread.
+            // Switch to the main thread - the call to AddCommand in AddResourcesCommand's constructor requires the UI thread.
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
 
-            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            var commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
             Instance = new AddResourcesCommand(package, commandService);
         }
-        #endregion 
 
-        #pragma warning disable VSTHRD100 // Avoid async void methods
+        #endregion
+        
+#pragma warning disable VSTHRD100 // Avoid async void methods
         private async void Execute(object sender, EventArgs e)
-        #pragma warning restore VSTHRD100 // Avoid async void methods
+#pragma warning restore VSTHRD100 // Avoid async void methods
         {
             try
             {
                 await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
 
-                keywordsCount = 0;
+                _keywordsCount = 0;
 
-                var dte = await ServiceProvider.GetServiceAsync(typeof(_DTE)) as _DTE;
-                if (dte == null) return;
+                if (!(await ServiceProvider.GetServiceAsync(typeof(_DTE)) is _DTE dte)) return;
 
                 var activeDocument = dte.ActiveDocument;
-                var source = activeDocument.Object() as TextDocument;
-                if (source == null) return;
+                if (!(activeDocument.Object() is TextDocument source)) return;
 
 
                 var text = source.CreateEditPoint(source.StartPoint).GetText(source.EndPoint);
                 var xDocument = XDocument.Parse(text);
-                if (xDocument == null) return;
 
 
                 var keywords = GetKeywords(xDocument);
                 if (keywords.Count == 0) return;
 
                 var projects = dte.Solution.Projects.GetEnumerator();
-               
+
 
                 while (projects.MoveNext())
                 {
-                    var items2 = ((Project)projects.Current).ProjectItems.GetEnumerator();
+                    var items2 = ((Project) projects.Current)?.ProjectItems.GetEnumerator();
 
-                    while (items2.MoveNext())
+                    while (items2 != null && items2.MoveNext())
                     {
-                        var item2 = (ProjectItem)items2.Current;
-                        projectItems.Add(GetFiles(item2));
+                        var item2 = (ProjectItem) items2.Current;
+                        _projectItems.Add(GetFiles(item2));
                     }
                 }
 
 
-                foreach (var file in projectItems)
-                    if (file.Name.Contains("Resources.resw"))
-                        WriteResources(keywords, file.FileNames[0]);
+                foreach (var file in _projectItems.Where(file =>
+                {
+                    ThreadHelper.ThrowIfNotOnUIThread();
+                    return file.Name.Contains("Resources.resw");
+                }))
+                    WriteResources(keywords, file.FileNames[0]);
+
 
                 ShowMessageBox(
-                    $"You added {keywordsCount} strings to Resources",
+                    $"You added {_keywordsCount} strings to Resources",
                     "Resources String Added",
                     OLEMSGICON.OLEMSGICON_INFO);
             }
             catch (Exception)
             {
- 
+                ShowMessageBox(
+                    "The extension stop working",
+                    "Critical error",
+                    OLEMSGICON.OLEMSGICON_CRITICAL);
             }
         }
 
-        private  void ShowMessageBox( string message, string title, OLEMSGICON type )
-        {
-            VsShellUtilities.ShowMessageBox(
-                          package,
-                         message,
-                          title,
-                          type,
-                          OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                          OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
 
-        }
-
-              ProjectItem GetFiles(ProjectItem item)
+        private ProjectItem GetFiles(ProjectItem item)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
-            //base case
-            if (item.ProjectItems == null)
-            {
-                return item;
-            }
+
+            if (item.ProjectItems == null) return item;
 
             var items = item.ProjectItems.GetEnumerator();
             while (items.MoveNext())
             {
-                var currentItem = (ProjectItem)items.Current;
-                projectItems.Add(GetFiles(currentItem));
+                var currentItem = (ProjectItem) items.Current;
+                _projectItems.Add(GetFiles(currentItem));
             }
 
             return item;
         }
 
-        private bool ResourcesExist(XDocument writeFileXml, string resourceName)
+        private void ShowMessageBox(string message, string title, OLEMSGICON type)
         {
-            foreach (var node in writeFileXml.Descendants())
-            {
-                if (node.Name.Equals(DATA))
-                {
-                    if (node.Attribute(NAME).Value.Equals(resourceName))
-                    {
-                        
-                        return true;
-                    }
-                }
-            }
-
-            return false;
+            VsShellUtilities.ShowMessageBox(
+                _package,
+                message,
+                title,
+                type,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
         }
-        public  void WriteResources(Dictionary<string, string> keywords, string path)
+
+        private static bool ResourcesExist(XContainer writeFileXml, string resourceName)
+        {
+            return writeFileXml.Descendants().Where(node => node.Name.Equals(Data))
+                .Any(node => node.Attribute(Name).Value.Equals(resourceName));
+        }
+
+        private static void WriteResources(Dictionary<string, string> keywords, string path)
         {
             try
             {
                 var writeFileXml = XDocument.Load(path);
 
-                foreach (KeyValuePair<string, string> item in keywords)
-                {
+                foreach (var item in keywords)
                     if (!ResourcesExist(writeFileXml, item.Key))
                     {
-                        var root = new XElement(DATA);
-                        root.Add(new XAttribute(NAME, item.Key));
-                        root.Add(new XAttribute(SPACE, "preserve"));
-                        root.Add(new XElement(VALUE, item.Value));
-                        root.Add(new XElement(COMMENT, ""));
-                        writeFileXml.Element(ROOT).Add(root);
-                       
+                        var root = new XElement(Data);
+                        root.Add(new XAttribute(Name, item.Key));
+                        root.Add(new XAttribute(Space, "preserve"));
+                        root.Add(new XElement(Value, item.Value));
+                        root.Add(new XElement(Comment, ""));
+                        writeFileXml.Element(Root)?.Add(root);
                     }
 
-                }
-
                 writeFileXml.Save(path);
-                 
             }
             catch (Exception e)
             {
-
                 Debug.WriteLine(e.StackTrace);
             }
-
         }
-        public Dictionary<string, string> GetKeywords(XDocument xDocument )
-        { 
-            Dictionary<string, string> keywords = new Dictionary<string, string>(); 
+
+        private Dictionary<string, string> GetKeywords(XContainer xDocument)
+        {
+            var keywords = new Dictionary<string, string>();
 
             foreach (var node in xDocument.Descendants())
-            {
-                if (node.Attribute(UID) != null)
-                {
+                if (node.Attribute(Uid) != null)
                     foreach (var attr in node.Attributes())
-                    {
-                        foreach (var item in HEADERS)
+                    foreach (var item in _headers.Where(item => attr.Name.Equals(item)))
+                        try
                         {
-                            if (attr.Name.Equals(item))
+                            if (node.Attribute(Uid).Value.Equals(attr.Value))
                             {
-                                try
-                                {
-                                    if (node.Attribute(UID).Value.Equals(attr.Value))
-                                        ShowMessageBox(
-                                                $"x:Uid value in node {node.Name?.LocalName} can't be empty",
-                                                $"Missing value",
-                                                OLEMSGICON.OLEMSGICON_CRITICAL);
-                                   else if (node.Attribute(UID).Value.Equals(attr.Value))
-                                        ShowMessageBox(
-                                                $"x:Uid value {node.Attribute(UID).Value} can't be the same as the value in {item.LocalName}",
-                                                $"Resource {node.Attribute(UID).Value} not added",
-                                                OLEMSGICON.OLEMSGICON_WARNING);
-                                    else
-                                    {
-                                        keywords.Add($"{node.Attribute(UID).Value}.{item.LocalName}", attr.Value);
-                                        keywordsCount++;
-                                    } 
-                                }
-                                catch (Exception)
-                                { 
-
-                                }
+                                ShowMessageBox(
+                                    $"x:Uid value in node {node.Name?.LocalName} can't be empty",
+                                    "Missing value",
+                                    OLEMSGICON.OLEMSGICON_CRITICAL);
                             }
-                        } 
-
-                    }
-
-                }
-
-            }
+                            else if (  node.Attribute(Uid).Value.Equals(attr.Value))
+                            {
+                                ShowMessageBox(
+                                    $"x:Uid value {node.Attribute(Uid)?.Value} can't be the same as the value in {item.LocalName}",
+                                    $"Resource {node.Attribute(Uid)?.Value} not added",
+                                    OLEMSGICON.OLEMSGICON_WARNING);
+                            }
+                            else
+                            {
+                                keywords.Add($"{node.Attribute(Uid)?.Value}.{item.LocalName}", attr.Value);
+                                _keywordsCount++;
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            //ignore
+                        }
 
 
             return keywords;
-
         }
+
+ 
     }
 }
